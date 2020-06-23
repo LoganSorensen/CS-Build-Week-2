a136d71503d1a490b5c32a10fce52598773c98a3
Logan Sorensen <loganmsorensen@gmail.com>
2020-06-23 13:45:19 -0700

    modified add-two-numbers solution, added day 2 challenges

--- leetcode-challenges/add-two-numbers.js
+++ leetcode-challenges/add-two-numbers.js
@@ -5,25 +5,31 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-
 /*
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
-
 var addTwoNumbers = function (l1, l2) {
   let sum = 0;
   let current = new ListNode(0);
   let result = current;
 
   while (l1 !== null || l2 !== null) {
-    sum += l1.val + l2.val;
-    l1 = l1.next;
-    l2 = l2.next;
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
 
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    // creates a new node with the value stored in sum, % 10 is used in the event that the sum is greater than 9
     current.next = new ListNode(sum % 10);
     current = current.next;
+    // if the current sum is greater than 9, carry the 1 into the next sum
     sum = sum > 9 ? 1 : 0;
   }
 
